119ab8e963f69f992ae8727e84e86942f8b04d05
simonkubis <simon.kubis20@gmail.com>
2025-12-23 01:27:23 +0100

    minor changes

--- experiment_1/main.py
+++ experiment_1/main.py
@@ -3,14 +3,37 @@ could_use = []
 always_use = []
 
 neutral_keywords = [
-            "see also", "used with", "apply before", "apply after", "related to", "related pattern", 
-            "related pattern", "similar pattern", "alternative to", "alternatively used pattern"
-        ]
+    "see also",
+    "used with",
+    "apply before",
+    "apply after",
+    "related to",
+    "related pattern",
+    "similar pattern",
+    "alternative to",
+    "alternatively used pattern"
+]
 
 negative_keywords = [
-            "do not use", "never use", "avoid using", "not recommended", "not to be used"
-        ]
+    "do not use",
+    "never use",
+    "avoid using",
+    "not recommended",
+    "not to be used"
+]
 
 positive_keywords = [
-            "always use", "must use", "recommended", "best practice", "should use", "this pattern uses", "pattern is an alternative"
-        ]
+    "always use",
+    "must use",
+    "recommended",
+    "best practice",
+    "should use",
+    "this pattern uses",
+    "pattern is an alternative"
+]
+
+KEYWORD_WEIGHTS = {
+    "negative": 0.0,
+    "neutral": 0.5,
+    "positive": 1.0
+}
